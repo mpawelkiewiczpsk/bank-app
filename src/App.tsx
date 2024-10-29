c0eeede9469a775b7d8e00888438412516fa5989
mpawelkiewiczpsk <mpawelkiewicz@tu.kielce.pl>
2024-10-29 14:45:44 +0100

    added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Button } from 'antd';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './views/error-page';
+import Contact from './views/contact';
+import Login from './views/login.tsx';
 
-const App = () => (
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: 'contacts/:contactId',
+          element: <Contact />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+
+  return (
     <div className="App">
-        <Button type="primary">Button</Button>
+      <RouterProvider router={router} />
     </div>
-);
+  );
+};
 
 export default App;
